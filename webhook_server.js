54ac4cd239694764effba6287389df9fd6ae4383
ashpool <ashpoolin@protonmail.com>
2023-02-03 12:39:59 -0700

    reduce console logging, maybe saves RAM

--- webhook_server.js
+++ webhook_server.js
@@ -16,28 +16,28 @@ const pool = new Pool({
 
 const insertParsedTransaction = (req) => {
     const data = req.body[0];
-    console.log("resp: ", data);
+    // console.log("resp: ", data);
     const LAMPORTS_PER_SOL = 1000000000;
     const slot = data.slot
     const blocktime = data.blockTime;
     const err = data.err;
     const fee = data.meta.fee / LAMPORTS_PER_SOL;
 
-    console.log(`slot: ${slot}`);
-    console.log(`blocktime: ${blocktime}`);
-    console.log(`err: ${err}`);
-    console.log(`fee: ${fee}`);
-    data.transaction.message.accountKeys.map((akey, index) => {
-        console.log(`accountKeys[${index}] : ${akey}`);
-    });
-    console.log(`header: ${JSON.stringify(data.transaction.message.header)}`);
+    // console.log(`slot: ${slot}`);
+    // console.log(`blocktime: ${blocktime}`);
+    // console.log(`err: ${err}`);
+    // console.log(`fee: ${fee}`);
+    // data.transaction.message.accountKeys.map((akey, index) => {
+    //     console.log(`accountKeys[${index}] : ${akey}`);
+    // });
+    // console.log(`header: ${JSON.stringify(data.transaction.message.header)}`);
     
     data.transaction.message.instructions.map(async (instruction, index) => {
         console.log(`instruction: ${JSON.stringify(instruction)}`);
-        console.log(`instruction.data: ${instruction.data}`);
+        // console.log(`instruction.data: ${instruction.data}`);
         
         if ( data.transaction.message.accountKeys[instruction.programIdIndex] == '11111111111111111111111111111111' && instruction.data.substring(0,2) == '3B') {
-            console.log("we made it!")
+            // console.log("we made it!")
             const program = 'system';
 
             // this relies on solana/web3.js PublicKey type
@@ -54,8 +54,8 @@ const insertParsedTransaction = (req) => {
             const source = `${data.transaction.message.accountKeys[instruction.accounts[0]]}`
             const destination = `${data.transaction.message.accountKeys[instruction.accounts[1]]}`
 
-            console.log(`source: ${source}`);
-            console.log(`destination: ${destination}`);
+            // console.log(`source: ${source}`);
+            // console.log(`destination: ${destination}`);
             // const destination = data.transaction.message.accountKeys[instruction.accounts[1]].toBase58();
             // const destination = bs58.encode((data.transaction.message.accountKeys[instruction.accounts[1]]).toBuffer('le', 8));
             const lamports = Number(( Buffer.from((bs58.decode(instruction.data)).slice(4,12)) ).readBigUInt64LE());
@@ -64,7 +64,7 @@ const insertParsedTransaction = (req) => {
             const signature = data.transaction.signatures[0];
             const type = 'transfer'
     
-            console.log(`${program},${type},${signature},${err},${slot},${blocktime},${fee},${source},${destination},${lamports},${decimals},${solAmount}`);
+            // console.log(`${program},${type},${signature},${err},${slot},${blocktime},${fee},${source},${destination},${lamports},${decimals},${solAmount}`);
             // const insertText = 'INSERT INTO webhooks_sol_event_log(program, type, signature, err, slot, blocktime, fee, source, destination, lamports, decimals, sol_amount) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';
             // await client.query(insertText, [program, type, signature, err, slot, blocktime, fee, source, destination, lamports, decimals, solAmount]);
             
@@ -75,7 +75,7 @@ const insertParsedTransaction = (req) => {
                     console.log("insert FAILED!");
                   }
                   resolve(results.rows);
-                  console.log("insert OK.");
+                  console.log(`inserted sig OK: ${signature}`);
             
                 })
               });
